bdd72992c684cc9644f6d324a9be1621a030d325
Ciceron67 <ciceron111185@gmail.com>
2024-07-16 14:03:04 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,7 @@
+completed_tasks = 12
+time_spent = 1.5
+course_name = 'Python'
+time_lesson = time_spent / completed_tasks
+print('Курс: ' + course_name + ', всего задач: ' + str(completed_tasks)
+      + ', затрачено часов: ' + str(time_spent)
+      + ', среднее время выполнения ' + str(time_lesson) + ' часа')
